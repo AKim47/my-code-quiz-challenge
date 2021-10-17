f9fd05cc8d30a0ae09947b182f3169ea2252018e
AKim47 <abel.kim.22@gmail.com>
2021-10-17 17:17:26 -0500

    added JS and tested button functionality

---
+++ assets/script.js
@@ -0,0 +1,12 @@
+var scores = document.querySelector("#scores");
+var start = document.querySelector("#generate");
+var timer = document.querySelector("#timer");
+
+scores.addEventListener("click", function(){
+    alert("scores button clicked");
+});
+
+start.addEventListener("click", function(){
+    alert("start button clicked");
+});
+
