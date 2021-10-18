96c2c44b84b2b811acbcc3cc830b7b7ec1a52805
AKim47 <abel.kim.22@gmail.com>
2021-10-17 21:02:21 -0500

    added 1st question functionality

--- assets/script.js
+++ assets/script.js
@@ -6,9 +6,10 @@ var h1E1 = document.createElement("h1");
 var h2E1 = document.createElement("h2");
 var startHTML = document.createElement("div");
 var startBtn = document.createElement("button");
+var question = 0;
 
-scoresBtn.textContent = "View High Scores";
-timerHTML.textContent = 75;
+scoresBtn.textContent = "View Scores";
+timerHTML.textContent = 35;
 h1E1.textContent = "Coding Quiz Challenge";
 h2E1.textContent = "Try and complete this quiz before time runs out! Every wrong answer deducts 10 seconds from your time/score.";
 startBtn.textContent = "Start Quiz";
@@ -27,14 +28,63 @@ startBtn.id = "generate";
 
 var scores = document.querySelector("#scores");
 var start = document.querySelector("#generate");
-var highScore = [];
+var highScore = [["Abel Kim", 20]];
 
 
 
 scores.addEventListener("click", function(){
+    body.innerHTML = "";
+    for (let i = 0; i < highScore.length; i++) {
+        console.log(highScore[i][0])
+    }
     alert("scores button clicked");
 });
 
 start.addEventListener("click", function(){
-    alert("start button clicked");    
+    question++;
+    body.innerHTML = "";
+
+    if (question === 1){
+        var q1H1 = document.createElement("h1");
+        var q1BtnWrapper = document.createElement("div");
+        var q1B1 = document.createElement("button");
+        var q1B2 = document.createElement("button");
+        var q1B3 = document.createElement("button");
+        var q1B4 = document.createElement("button");
+        q1H1.textContent = "What do you use to copy an existing GitHub Repo to your local drive?"
+        q1B1.textContent = "Git Clone";
+        q1B2.textContent = "Git Pull";
+        q1B3.textContent = "Git Commit";
+        q1B4.textContent = "Git Add";
+        body.appendChild(timerHTML);
+        body.appendChild(q1H1);
+        body.appendChild(q1BtnWrapper);
+        q1BtnWrapper.appendChild(q1B1);
+        q1BtnWrapper.appendChild(q1B2);
+        q1BtnWrapper.appendChild(q1B3);
+        q1BtnWrapper.appendChild(q1B4);
+        q1B1.id = "btn";
+        q1B2.id = "btn";
+        q1B3.id = "btn";
+        q1B4.id = "btn";
+        const buttons = document.querySelectorAll('#btn')
+        buttons.forEach(function(currentBtn){
+            currentBtn.addEventListener('click', function(){
+                if (currentBtn.textContent === "Git Clone"){
+                    var ans = document.createElement("p");
+                    ans.textContent = "Correct";
+                    body.appendChild(ans);
+                }
+                else{
+                    timerHTML.textContent = timerHTML.textContent - 10;
+                    var ans = document.createElement("p");
+                    ans.textContent = "Incorrect";
+                    body.appendChild(ans);
+                }
+                question++;
+            })
+        })
+    }
+
+
 });
